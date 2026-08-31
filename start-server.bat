203a4f3c0359f48770a1a8227d4801b50db9ffb8
Tassuu7 <developer@edupulse.edu>
2026-08-31 17:28:35 +0530

    chore: add start-server.bat Windows one-click launcher

---
+++ start-server.bat
@@ -0,0 +1,13 @@
+@echo off
+title EduPulse Education CRM Server
+echo ================================================================
+echo           EDUPULSE ENTERPRISE EDUCATION CRM SYSTEM               
+echo ================================================================
+echo Starting local web server...
+echo.
+echo Server will be accessible at:
+echo   --^> http://localhost:4050
+echo   --^> http://127.0.0.1:4050
+echo.
+node server.js
+pause
